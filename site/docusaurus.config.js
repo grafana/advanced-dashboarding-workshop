00dc65f9b408a2cf262187264e1263c89ea8bae2
Tom Donohue <monodot@gmail.com>
2026-05-05 10:54:50 +0100

    make baseUrl configurable with BASE_URL environment variable

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://grafana.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/advanced-dashboarding-workshop/',
+  baseUrl: process.env.BASE_URL ?? '/advanced-dashboarding-workshop/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
